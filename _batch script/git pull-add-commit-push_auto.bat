4b789a89b30d149d582cf77c7c35589288f0a442
wrist0neye <did96123@gmail.com>
2024-08-08 01:56:58 +0900

    blog update at 2024-08-08 T 01:56:56

--- _batch script/git pull-add-commit-push_auto.bat
+++ _batch script/git pull-add-commit-push_auto.bat
@@ -1,38 +1,39 @@
 @echo off
+cd ..
 set "currentDate=%date%"
 set "currentTime=%time%"
-  
+
 for /f "tokens=1-4 delims=/- " %%a in ("%currentDate%") do (
-    set "year=%%a"
-    set "month=00%%b"
-    set "day=00%%c"
+    set "year=%%a"
+    set "month=00%%b"
+    set "day=00%%c"
 )
-  
+
 set month=%month:~-2%
 set day=%day:~-2%
 set "formattedDate=%year%-%month%-%day%"
-  
+
 for /f "tokens=1-3 delims=:., " %%a in ("%currentTime%") do (
-    set "hour=00%%a"
-    set "minute=00%%b"
-    set "second=00%%c"
+    set "hour=00%%a"
+    set "minute=00%%b"
+    set "second=00%%c"
 )
-  
+
 set hour=%hour:~-2%
 set minute=%minute:~-2%
 set second=%second:~-2%
 set "formattedTime=%hour%:%minute%:%second%"
 
-:: 코멘트 마지막 확인
-echo "blog update at %formattedDate% at %formattedTime%"
-	
-pause
+echo "[Comment]blog update at %formattedDate% T %formattedTime%"
+
 @echo on
+pause
+
 
 git add -A
-git commit -m "blog update at %formattedDate% at %formattedTime%"
-  
+git commit -m "blog update at %formattedDate% T %formattedTime%"
+
 pause
-  
+
 git push origin main
 pause
